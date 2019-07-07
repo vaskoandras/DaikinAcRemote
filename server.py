306758de58562b73bceed0ae7f74eabd090da395
Fufi <vaskoandrasgmail.com>
2019-07-07 19:17:32 +0200

    monkey path before app import

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask_app import app
 from gevent import monkey; monkey.patch_all()
 from gevent.pywsgi import WSGIServer
+
+from flask_app import app
 
 http_server = WSGIServer(('', 5000), app)
 http_server.serve_forever()
